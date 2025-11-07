9c95d1561c8e6647a11dd8f35da81ababa57199c
Luis Cobian <coby10@gmail.com>
2025-11-07 13:21:42 -0600

    Inicial del proyecto

---
+++ src/main/java/mx/edu/tecmm/elgrullo/ventitaapp/VentitaApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.edu.tecmm.elgrullo.ventitaapp;
+
+/**
+ *
+ * @author coby_
+ */
+public class VentitaApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
